a21d28de9d21b9c720cd6405383fa621e209f99a
Mitya Eremeev <dmitry.eremeev@kaspersky.com>
2021-02-17 20:46:33 +0300

    [WIP] Base rest api routes.

--- core.py
+++ core.py
@@ -19,10 +19,68 @@ def get_token(user, password, domain_id="default"):
         }
     }
 
-    keystone_response = requests.post(f"http://{config.openstack_ip}/identity/v3/auth/tokens?nocatalog", json=data)
+    keystone_response = requests.post(url=f"http://{config.openstack_ip}/identity/v3/auth/tokens?nocatalog", json=data)
     result = dict()
     if keystone_response.status_code != 201:
         result["error"] = f"keystone error: {keystone_response.text}"
     else:
         result["token"] = keystone_response.headers["X-Subject-Token"]
     return result
+
+
+def get_flavors(token):
+    response = requests.get(url=f"http://{config.openstack_ip}/compute/v2.1/flavors/detail",
+                            headers={"X-Auth-Token": token})
+    return {"status": response.status_code, "data": response.json()}
+
+
+def get_images(token):
+    response = requests.get(url=f"http://{config.openstack_ip}/image/v2/images", headers={"X-Auth-Token": token})
+    return {"status": response.status_code, "data": response.json()}
+
+
+def get_networks(token):
+    response = requests.get(url=f"http://{config.openstack_ip}:9696/v2.0/networks", headers={"X-Auth-Token": token})
+    return {"status": response.status_code, "data": response.json()}
+
+
+def get_virtual_machines(token):
+    response = requests.get(url=f"http://{config.openstack_ip}/compute/v2.1/servers/detail",
+                            headers={"X-Auth-Token": token})
+    return {"status": response.status_code, "data": response.json()}
+
+
+def create_virtual_machine(token, flavor_name, image_name, network_id, virtual_machine_name):
+    all_flavors = get_flavors(token)
+    if all_flavors["status"] != 200:
+        return {"status": all_flavors["status"], "error": f"get flavor list error: {all_flavors['error']}"}
+    flavor_ref = None
+    for flavor_item in all_flavors["data"]["flavors"]:
+        if flavor_item["name"] == flavor_name:
+            flavor_ref = flavor_item["id"]
+            break
+    if flavor_ref is None:
+        return {"status": 404, "error": f"flavor {flavor_name} not found"}
+
+    all_images = get_images(token)
+    if all_images["status"] != 200:
+        return {"status": all_images["status"], "error": f"get image list error: {all_images['error']}"}
+    image_ref = None
+    for image_item in all_images["data"]["images"]:
+        if image_item["name"] == image_name:
+            image_ref = image_item["id"]
+            break
+    if image_ref is None:
+        return {"status": 404, "error": f"image {image_name} not found"}
+    response = requests.post(url=f"http://{config.openstack_ip}/compute/v2.1/servers",
+                             headers={"X-Auth-Token": token},
+                             json={"server": {
+                                 "name": virtual_machine_name,
+                                 "imageRef": image_ref,
+                                 "flavorRef": flavor_ref,
+                                 "min_count": 1,
+                                 "max_count": 1,
+                                 "networks": [{"uuid": network_id}]
+                                }
+                             })
+    return {"status": response.status_code, "data": response.json()}
--- rest_api.py
+++ rest_api.py
@@ -26,15 +26,54 @@ def auth():
     return decorator
 
 
-@app.route('/api/token')
+@app.get('/api/token')
 @auth()
 def get_token():
     return bottle.HTTPResponse(body={"token": bottle.request.token})
 
 
-@app.route('/api/flavors')
+@app.get('/api/flavors')
+@auth()
 def get_flavors():
-    pass
+    result = core.get_flavors(token=bottle.request.token)
+    return bottle.HTTPResponse(status=result["status"], body=result["data"])
+
+
+@app.get('/api/images')
+@auth()
+def get_images():
+    result = core.get_images(token=bottle.request.token)
+    return bottle.HTTPResponse(status=result["status"], body=result["data"])
+
+
+@app.get('/api/networks')
+@auth()
+def get_networks():
+    result = core.get_networks(token=bottle.request.token)
+    return bottle.HTTPResponse(status=result["status"], body=result["data"])
+
+
+@app.get('/api/virtual_machines')
+@auth()
+def get_virtual_machines():
+    result = core.get_virtual_machines(token=bottle.request.token)
+    return bottle.HTTPResponse(status=result["status"], body=result["data"])
+
+
+@app.post('/api/virtual_machines')
+@auth()
+def create_virtual_machine():
+    absent_fields = list(filter(lambda required_field: required_field not in bottle.request.json,
+                                ["flavor_name", "image_name", "network_id", "virtual_machine_name"]))
+    if absent_fields:
+        return bottle.HTTPResponse(status=400,
+                                   body={"error": f"the following fields were not provided: {absent_fields}"})
+    result = core.create_virtual_machine(token=bottle.request.token,
+                                         flavor_name=bottle.request.json["flavor_name"],
+                                         image_name=bottle.request.json["image_name"],
+                                         network_id=bottle.request.json["network_id"],
+                                         virtual_machine_name=bottle.request.json["virtual_machine_name"])
+    return bottle.HTTPResponse(status=result["status"], body=result["data"])
 
 
 bottle.run(app, host="192.168.0.104", port=8081, debug=True)
